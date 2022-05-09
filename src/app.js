9907f1d28718c2565313eaa2816ada76dc57a11c
ezfood3 <ezfood3@naver.com>
2022-05-09 14:00:17 +0900

    src분류

---
+++ src/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// 모듈
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const home = require('../src/routes/home');
+
+// 앱 셋팅
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', home); 
+
+module.exports = app;
+
+// app.listen(3000, () => {
+//   console.log('3000번에서 실행');
+// })
